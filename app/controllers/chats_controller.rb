f9e547a093ac3abdfce86f1ff270bd531c70390d
chilidoggca <jack.chili.lee@gmail.com>
2018-06-20 22:12:51 -0700

    removed faraday middleware gem

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -1,10 +1,14 @@
 class ChatsController < ApplicationController
   def post_message
-    conn = Faraday.new(:url => 'https://www.googleapis.com')
+    conn = Faraday.new(:url => 'https://www.googleapis.com') do |builder|
+      builder.request :oauth2, 'access_token'
+    end
     response = conn.post do |req|
       req.url '/youtube/v3/liveChat/messages'
       req.params['part'] = 'snippet'
-      req.params['key'] = ENV['GOOGLE_API_KEY']
+      req.headers = {
+        "Authorization": 'access_token'
+      }
       req.body = {
         "snippet": {
           "liveChatId": "Cg0KC0NfdGdLLV9YQ0Yw",
@@ -17,13 +21,15 @@ class ChatsController < ApplicationController
     end
     if response.success?
       puts "#{response.success?}=================="
-      data = JSON.parse(response.body)
-      puts data
+      # data = JSON.parse(response.body)
+      # puts data
+      puts response.body
       redirect_to streams_path
     else
       puts 'failed!!!!!!!!!!!!!!!!!!!!!!!'
-      data = JSON.parse(response.body)
-      puts data
+      puts response.body
+      # data = JSON.parse(response.body)
+      # puts data
       redirect_to streams_path
     end
   end
