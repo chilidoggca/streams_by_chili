787b037b1d9bd8a93b0d7cfef2675276826799e7
chilidoggca <jack.chili.lee@gmail.com>
2018-06-21 15:46:35 -0700

    installed react and point root to client controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+
+  match '/client/*path', to: 'client#index', via: :all
+  match '/client/', to: 'client#index', via: :all
+
   # devise_for :users, path: 'auth', path_names: { sign_in: 'login', sign_out: 'logout', password: 'secret', confirmation: 'verification', unlock: 'unblock', registration: 'register', sign_up: 'sign_up' }
   devise_for :users, skip: [:sessions], :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
 
@@ -8,13 +12,13 @@ Rails.application.routes.draw do
     get 'sign_out', :to => 'devise/sessions#destroy', :as => :destroy_user_session
   end
 
-  root :to => "streams#index"
+  root :to => "client#index"
 
   resources :streams, only: :index
 
   # get 'chats/post_message'
   get 'chats/get_messages'
-  
+
   get 'messages', :to => 'messages#index'
   get 'messages/author', :to => 'messages#author'
   get 'messages/chat', :to => 'messages#chat'
