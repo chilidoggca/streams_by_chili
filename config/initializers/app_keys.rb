32f677a61e19a1ad8adf6f1e59d9f4bc7851b54c
chilidoggca <jack.chili.lee@gmail.com>
2018-06-20 17:06:51 -0700

    faraday to get live streams

---
+++ config/initializers/app_keys.rb
@@ -0,0 +1,1 @@
+ENV['GOOGLE_API_KEY'] = 'AIzaSyAFp-yv2tH4ngSRqRwwt53M4FVGf_4s134'
--- config/initializers/test_youtube.rb
+++ config/initializers/test_youtube.rb
@@ -1,5 +1,30 @@
 puts "===========faraday test================="
 
-conn = Faraday.new(:url => 'http://www.youtube.com')
-response = conn.get '/'                 # GET http://www.example.com/users'
-puts response
+#   req.params['id'] = 'UC4R8DWoMoI7CAwX8_LjQHig' #live channel id
+
+conn = Faraday.new(:url => 'https://www.googleapis.com')
+response = conn.get do |req|
+  req.url '/youtube/v3/search'
+  req.params['part'] = 'snippet'
+  req.params['eventType'] = 'live'
+  req.params['resultsPerPage'] = '10'
+  req.params['maxResults'] = '50'
+  req.params['type'] = 'video'
+  req.params['videoCategoryId'] = '20'
+  req.params['key'] = ENV['GOOGLE_API_KEY']
+end
+data = JSON.parse(response.body)
+puts data
+# data.each do |i, j|
+#   if i == 'id'
+#     puts "======================================="
+#     puts 'videoId: '
+#     puts j.videoId
+#   end
+# end
+# data.each do |i, j|
+#   if i == 'items'
+#     puts i
+#     puts j
+#   end
+# end
