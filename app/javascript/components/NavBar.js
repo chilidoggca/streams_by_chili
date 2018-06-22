f7d4aa2fadbffe16196afa5784fea42e1e4bd06d
chilidoggca <jack.chili.lee@gmail.com>
2018-06-21 19:20:42 -0700

    add logo image to navbar

--- app/javascript/components/NavBar.js
+++ app/javascript/components/NavBar.js
@@ -11,10 +11,14 @@ function NavBar (props) {
         height: '55px'
       }}
     >
-      <div className="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul className="navbar-nav mr-auto">
+      <a className="navbar-brand" href="#"><img src="streamsbychili.png" style={{height: '45px'}} /></a>
+      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon"></span>
+      </button>
+      <div className="collapse navbar-collapse bg-dark" id="navbarSupportedContent">
+        <ul className="navbar-nav ml-auto">
           <li className="nav-item">
-            <Link style={{marginRight: '20px'}} to="/">&nbsp;Streams by Chili&nbsp;</Link>
+            <Link style={{marginRight: '20px'}} to="/">Home</Link>
           </li>
           <li className="nav-item">
             <Link style={{marginRight: '20px'}} to="/live">Live Streams</Link>
