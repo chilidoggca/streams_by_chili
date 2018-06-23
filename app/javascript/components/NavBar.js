66206111b957af00a366117a9104547684a36589
chilidoggca <jack.chili.lee@gmail.com>
2018-06-23 01:36:15 -0700

    updated home page and navbar styling

--- app/javascript/components/NavBar.js
+++ app/javascript/components/NavBar.js
@@ -13,16 +13,16 @@ function NavBar (props) {
     >
       <div className="container">
         <Link className="navbar-brand" to="/"><img src="streamsbychili.png" style={{height: '45px'}} /></Link>
-        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
+        <button className="navbar-toggler collapsed" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          {' '}
         </button>
         <div className="collapse navbar-collapse bg-dark" id="navbarSupportedContent">
           <ul className="navbar-nav ml-auto">
             <li className="nav-item">
-              <Link style={{marginLeft: '30px'}} to="/streams">Live Streams</Link>
+              <Link style={{marginLeft: '50px'}} to="/streams">Live Streams</Link>
             </li>
             <li className="nav-item">
-              <Link style={{marginLeft: '30px'}} to="/messages">Chat Messages</Link>
+              <Link style={{marginLeft: '50px'}} to="/messages">Chat Messages</Link>
             </li>
           </ul>
         </div>
--- app/javascript/components/pages/HomePage.js
+++ app/javascript/components/pages/HomePage.js
@@ -6,7 +6,45 @@ function HomePage (props) {
       className='HomePage'
       style={{padding: '60px 20px 20px'}}
     >
-      <h1 style={{position: 'absolute', top: '50%', right: '50px'}}>Welcome to Streams by Chili</h1>
+      <div className="welcome" style={{position: 'absolute',
+        top: '50%', right: '50px', paddingLeft: '100px', textAlign: 'right'
+      }}>
+        <h1 style={{display: 'inline-block', color: 'rgba(0,0,0,0.8)'}}>
+          <span style={{fontSize: '0.8em'}}>Welcome to</span>
+          <span style={{fontSize: '1.25em'}}> Streams </span>
+        </h1>
+        <span style={{fontSize: '2em', whiteSpace: 'nowrap'}}>&nbsp;
+          <svg viewBox="0 0 200 40" style={{width: '200px', position: 'relative', bottom: '-10px'}}>
+            <defs>
+              <mask id="bychili" x="0" y="0" width="200" height="40">
+                <rect x="0" y="0" width="200" height="40" fill="#ffffff"/>
+                <text textAnchor="middle" x="100" y="29" letterSpacing="10">
+                  by CHILI
+                </text>
+              </mask>
+            </defs>
+            <rect x="0" y="0" width="200" height="40" mask="url(#bychili)" fillOpacity="0.8"/>
+          </svg>
+        </span>
+      </div>
+      <div style={{background: 'url("pixels2.png") repeat scroll left top',
+        zIndex: '-19', position: 'absolute', top: '0',
+        left: '0', width: '100%', height: '100%'
+      }}>
+        &nbsp;
+      </div>
+      <div style={{background: 'url("leagueoflegends.jpg")',
+        backgroundPosition: '50% 50%', backgroundSize: 'cover',
+        zIndex: '-20', position: 'absolute', top: '0',
+        left: '0', width: '100%', height: '100%', opacity: '0.75'
+      }}>
+        &nbsp;
+      </div>
+      <div style={{backgroundColor: '#7FFFD4', zIndex: '-21',
+        position: 'absolute', top: '0', left: '0', width: '100%', height: '100%'
+      }}>
+        &nbsp;
+      </div>
     </main>
   );
 }
