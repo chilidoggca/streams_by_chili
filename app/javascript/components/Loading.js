efe0ced88c704dfc3333447a331bf729fcbd8c43
chilidoggca <jack.chili.lee@gmail.com>
2018-06-24 13:06:43 -0700

    added loading text animation

--- app/javascript/components/Loading.js
+++ app/javascript/components/Loading.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 function Loading (props) {
   return (
-    <div className='LoadingPage'>
-      <h2>Loading {props.loadingContent}...</h2>
+    <div className='Loading'>
+      <h5>loading {props.loadingContent}...</h5>
     </div>
   );
 }
